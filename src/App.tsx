250838697f3a504cb09f0f6a2980d9983adb44aa
cliffgor <clierozigy@gmail.com>
2022-05-01 20:28:01 +0300

    State created a state funtion for the search and result functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import React, {useState} from 'react';
+
+import logo from './logo.svg';
+
 function App() {
+  const [resultsFound, setResultsFound] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const searchResults = async (query: string) => {
+    const result = await fetch(`https://swapi.dev/api/films?search=${query}`)
+    return (await result.json()).results
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Skywalker</h1>
     </div>
   );
 }
