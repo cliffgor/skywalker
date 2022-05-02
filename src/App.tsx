2d14316439c6fbb66780a29008a7a34d727f8fba
cliffgor <clierozigy@gmail.com>
2022-05-02 12:13:21 +0300

    Functions Created functions and added a useEffect hook to handle the searching of films from a search box

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 import './App.css';
 
-import React, {useState} from 'react';
+import React, {FormEvent, useEffect, useState} from 'react';
 
 import logo from './logo.svg';
 
 function App() {
   const [resultsFound, setResultsFound] = useState([]);
-  const [search, setSearch] = useState('');
+  const [searchSkywalker, setSearchSkywalker] = useState('');
 
-  const searchResults = async (query: string) => {
+  const searchResults = async (query: string): Promise<any> => {
     const result = await fetch(`https://swapi.dev/api/films?search=${query}`)
     return (await result.json()).results
   }
 
+  const search = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const input = form.querySelector('#searchText') as HTMLInputElement;
+    setSearchSkywalker(input.value)
+  }
 
 
+useEffect(() => {
+  (async () => {
+    const query = encodeURIComponent(searchSkywalker);
+    if (query){
+      const response = await searchResults(query);
+      setResultsFound(response);
+    } 
+  })();
+},[searchSkywalker]);
   return (
     <div className="App">
       <h1>Welcome to Skywalker</h1>
+      <form  className="searchForm" onSubmit={event => search(event)}>
+        <input id="searchText" type="text" />
+        <button>Search </button>
+      </form>
     </div>
   );
 }
